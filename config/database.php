0ffb2eec8eec8364b34216715d9a4743d22e6c2f
Vincent Zhang <ivincent.zhang@gmail.com>
2020-10-04 11:43:03 +0800

    Do not put cakephp to root vendor directory

--- config/database.php
+++ config/database.php
@@ -8,18 +8,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'vincent',
 		'password' => 'ViVopu__zpoooiq@vincent',
-		'database' => 'monitor_ui_0_1_0',
-		'encoding'=> 'utf8',
-		'prefix' => ''
-	);
-
-	public $default__ = array(
-		'driver' => 'mysqli',
-		'persistent' => true,
-		'host' => 'qiwur.com',
-		'login' => 'monitor_ui',
-		'password' => 'VoVon__i8ooie@monitor_ui',
-		'database' => 'monitor_ui_0_1_0',
+		'database' => 'official_website_0_0_1',
 		'encoding'=> 'utf8',
 		'prefix' => ''
 	);
