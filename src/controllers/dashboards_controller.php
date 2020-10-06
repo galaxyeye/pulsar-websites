79fcba4257b6d0cb15bc3d6cce5ba8034511e18d
Vincent Zhang <ivincent.zhang@gmail.com>
2020-10-06 20:43:05 +0800

    Better experience to edit

--- src/controllers/dashboards_controller.php
+++ src/controllers/dashboards_controller.php
@@ -2,6 +2,16 @@
 
 class DashboardsController extends AppController
 {
-	public function index() {
-	}
+
+    public $uses = array(
+        'Dashboard',
+        'Solution'
+    );
+
+    public function index()
+    {
+        $faqs = json_decode(file_get_contents(APP_DATA . 'faqs.json', true), true);
+        $solutions = $this->Solution->find('all');
+        $this->set(compact('faqs', 'solutions'));
+    }
 }
