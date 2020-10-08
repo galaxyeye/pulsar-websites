9e5d34ad6543f724c15550bb7edc4eba8c65709a
Vincent Zhang <ivincent.zhang@gmail.com>
2020-10-08 15:00:57 +0800

    Multiple candidate databases

--- bin/tools/authorize.sh
+++ bin/tools/authorize.sh
@@ -3,5 +3,17 @@
 bin=$(dirname "$0")
 bin=$(cd "$bin">/dev/null || exit; pwd)
 
+config=$(cd "$bin/../../config">/dev/null || exit; pwd)
+if [ -e "$config/private/database.php" ]; then
+  cp "$config/private/database.php" "$config"
+else
+  cp "$config/public/database.php" "$config"
+fi
+
+if [ ! -e "$config/database.php" ]; then
+  echo "Database is not configured"
+  exit 0;
+fi
+
 bin/cake acl_extras aco_update
 bin/cake privilege_grantor
